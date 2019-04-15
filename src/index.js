86746fd0f18da0e0c65db9345af343034d3e13c2
sebastian <sebastian@marlon.be>
2019-04-15 17:39:10 +0200

    improve base page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Search from './Components/Search';
+
 const Index = () => {
-    return <div>Hello react!</div>;
+    return <div>
+        <h1>Accessibility Check</h1>
+        <p>Identify and fix common problems that affect your site's performance, accessibility, and user experience.</p>
+
+        <Search />
+
+    </div>;
 }
 
-ReactDOM.render(<Index />, document.getElementById('index'));
+ReactDOM.render(<Index />, document.getElementById('root'));
