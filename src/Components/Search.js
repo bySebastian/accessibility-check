6811c14d3a0face03997a3ae803a5eded102ad19
sebastian <sebastian@marlon.be>
2019-04-16 12:09:34 +0200

    add basic URL validation to search component

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -6,21 +6,41 @@ class Search extends Component {
         super(props);
 
         this.state = {
-            value: ''
+            websiteUrl: ''
         }
 
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(e) {
+        this.setState({ websiteUrl: e.target.value });
     }
 
     handleSubmit(e) {
-        // handle submit
+        e.preventDefault();
+        if (this.validateUrl(this.state.websiteUrl)) {
+            console.log('url', this.state.websiteUrl);
+        } else {
+            console.log('No valid URL!')
+        }
     }
+
+    validateUrl(url) {
+        var pattern = new RegExp('^(https?:\\/\\/)?'+           // protocol
+          '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+   // domain name
+          '((\\d{1,3}\\.){3}\\d{1,3}))'+                        // OR ip (v4) address
+          '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+                    // port and path
+          '(\\?[;&a-z\\d%_.~+=-]*)?'+                           // query string
+          '(\\#[-a-z\\d_]*)?$','i');                            // fragment locator
+        return !!pattern.test(url);
+      }
 
     render () {
         return (
             <form onSubmit={this.handleSubmit}>
                 <label htmlFor="websiteUrl">Page URL</label>
-                <input type="text" id="websiteUrl" name="websiteUrl" placeholder="https://www.google.com" defaultValue={ this.state.value } />
+                <input type="text" id="websiteUrl" name="websiteUrl" placeholder="https://www.google.com" defaultValue={ this.state.websiteUrl } onChange={ this.handleInputChange } />
                 <button type="sumbit">Search</button>
             </form>
             );
